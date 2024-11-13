f0236ae49cba565dadb95536adf0f4544f6ee107
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2024-11-13 05:49:02 -0300

    :tada: init: First implementation

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,11 +1,32 @@
-const menuButton = document.getElementById("menuButton");
-const closeMenuButton = document.getElementById("closeMenuButton");
-const mobileMenu = document.getElementById("mobileMenu");
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const headerResponse = await fetch("/src/components/header.html");
+    const headerData = await headerResponse.text();
+    document.querySelector(".header").innerHTML = headerData;
+    initMenu();
 
-menuButton.addEventListener("click", () => {
-  mobileMenu.classList.remove("translate-x-full"); // Mostra o menu
+    const footerResponse = await fetch("/src/components/footer.html");
+    const footerData = await footerResponse.text();
+    document.querySelector(".footer").innerHTML = footerData;
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
 
-closeMenuButton.addEventListener("click", () => {
-  mobileMenu.classList.add("translate-x-full"); // Oculta o menu
-});
+function initMenu() {
+  const menuButton = document.querySelector("#menuButton");
+  const mobileMenu = document.querySelector("#mobileMenu");
+  const closeMenuButton = document.querySelector("#closeMenuButton");
+
+  if (menuButton && mobileMenu && closeMenuButton) {
+    menuButton.addEventListener("click", () => {
+      mobileMenu.classList.remove("translate-x-full");
+    });
+    closeMenuButton.addEventListener("click", () => {
+      mobileMenu.classList.add("translate-x-full");
+    });
+  }
+}
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "background": "#F2F2F2",
+        "background-light": "#F5F5F5",
+        "background-dark": "#111111",
+        "text-primary": "#101010",
+        "text-secondary": "#ffffff",
+        "text-muted": "#888888",
+      },
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif'],
+        Oswald: ['Oswald', 'sans-serif'],
+        Montserrat: ['Montserrat', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
-}
-
+};
